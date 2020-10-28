e9a4645030affdf9358ad1739b1e489dac2bc75d
2oneweek <dlgkswn885@korea.ac.kr>
2020-10-28 17:30:53 +0900

    [BE] Feat: Sequelize 설치, DB init
    1. Sequelize, Sequelize-cli를 설치 (npm install 다시해야함)
    2. Sequelize init을 이용하여 DB init 및 config 설정

---
+++ server/db/models/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const config = {
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  operatorAlias: false,
+  dialect: "mysql",
+  timezone: "+09:00",
+};
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+
+//npx sequlize-cli model:generate --name user --attributes userid:integer
