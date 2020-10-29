f7a8095679f0feb1e18c18c50372aaa1f56212d3
dochoi <dochoi@student.42seoul.kr>
2020-10-29 16:38:07 +0900

    [iOS] Feat: Network Layer 구현
    네트워크 요청의 정보를 추상화해 RequestType 프로토콜 생성
    http Metohd를 구분할 RequestMethod Enum 생성
    네트워크 통신을 담당할 NetworkServiceProviding 프로토콜 생성
    네트워크 에러를 구분할 NetworkError Enum 생성
    네트워크 통신을 담당할 객체 NetworkService class 구현
    
     #9

---
+++ iOS/IssueTracker/IssueTracker/Common/Model/NetworkRequest.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkRequest.swift
+//  IssueTracker
+//
+//  Created by 최동규 on 2020/10/29.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+protocol RequestType {
+    var url: URL { get }
+    var method: RequestMethod { get }
+}
