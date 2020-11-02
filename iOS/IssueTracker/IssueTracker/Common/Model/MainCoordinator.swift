a186a2cea7d55c35f3e2e4024bc01428ac8d5d10
dochoi <dochoi@student.42seoul.kr>
2020-11-02 20:35:02 +0900

    [iOS] Feat: Coordinator 패턴 적용
    ViewController 생성시 의존성 주입을 위해 Coordinator 패턴을 적용하였습니다.
    
     #71

---
+++ iOS/IssueTracker/IssueTracker/Common/Model/MainCoordinator.swift
@@ -0,0 +1,41 @@
+//
+//  MainCoordinator.swift
+//  IssueTracker
+//
+//  Created by 최동규 on 2020/11/02.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    private let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    private let networkService: NetworkServiceProviding
+    
+    init(navigationController: UINavigationController, networkService: NetworkServiceProviding) {
+        self.navigationController = navigationController
+        self.networkService = networkService
+    }
+    
+    func start() {
+        let viewContoller = storyboard.instantiateViewController(
+            identifier: LoginViewController.identifier,
+            creator: { [unowned self] coder -> LoginViewController? in
+                return LoginViewController(coder: coder, useCase: LoginUseCase(networkService: self.networkService))
+            }
+        )
+        viewContoller.coordinator = self
+        navigationController.pushViewController(viewContoller, animated: true)
+    }
+    
+    func showSignUp() {
+        let viewContoller = storyboard.instantiateViewController(
+            identifier: SignUpViewController.identifier,
+            creator: { [unowned self] coder -> SignUpViewController? in
+                return SignUpViewController(coder: coder, useCase: SignUpUseCase(networkService: self.networkService))
+            }
+        )
+        viewContoller.coordinator = self
+        navigationController.pushViewController(viewContoller, animated: true)
+    }
+}
--- iOS/IssueTracker/IssueTracker/SignUpScene/Controller/SignUpViewController.swift
+++ iOS/IssueTracker/IssueTracker/SignUpScene/Controller/SignUpViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 final class SignUpViewController: UIViewController {
     
-    private let signUpUseCase: SignUpUseCase = SignUpUseCase(networkService: NetworkService())
+    static var identifier: String {
+        return String(describing: Self.self)
+    }
+    private let signUpUseCase: SignUpUseCase
+    weak var coordinator: MainCoordinator?
+    
+    init?(coder: NSCoder, useCase: SignUpUseCase) {
+        signUpUseCase = useCase
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("This viewController must be init with useCase.")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
