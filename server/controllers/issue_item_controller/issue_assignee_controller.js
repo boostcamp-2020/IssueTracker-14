8712a237b2a82d931bd179b2093e6a3027d94427
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-02 22:22:17 +0900

    [BE] Fix: passportConfig -> passport_config로 변환

--- server/controllers/issue_item_controller/issue_assignee_controller.js
+++ server/controllers/issue_item_controller/issue_assignee_controller.js
@@ -3,6 +3,8 @@ const { assignee: AssigneeModel } = require("../../db/models");
 
 const createAssignee = async (req, res) => {
   try {
+    const { issueid } = req.params;
+    const { id: userid } = req.user;
   } catch (error) {
     return res.status(400).json({ message: "fail", erroror: error.message });
   }
