b2dfd393b24a8f4939b8cb82042d9f6406a0d172
TTOzzi <devttozzi@gmail.com>
2020-11-02 18:57:42 +0900

    [iOS] Feat: 입력확인 결과에 따른 뷰 상태 변화 구현
    InputView의 입력이벤트 처리를 위한 InputViewDelegate 구현
    PatternChecker가 SignUpInfo를 갖고있음 -> 추후 회원가입 요청에 사용할 예정
    InputView에 텍스트가 입력되면, PatternChecker로 입력값을 검증하고, 그 결과값으로 뷰를 업데이트 하는 구조
    
    #35

--- iOS/IssueTracker/IssueTracker/SignUpScene/Controller/SignUpViewController.swift
+++ iOS/IssueTracker/IssueTracker/SignUpScene/Controller/SignUpViewController.swift
@@ -9,9 +9,43 @@ import UIKit
 
 final class SignUpViewController: UIViewController {
     
-    private let signUpUseCase: SignUpUseCase = SignUpUseCase(networkService: NetworkService())
+    @IBOutlet private weak var emailInputView: InputView!
+    @IBOutlet private weak var passwordInputView: InputView!
+    @IBOutlet private weak var passwordConfirmInputView: InputView!
+    @IBOutlet private weak var nameInputView: InputView!
+    @IBOutlet private weak var completeButton: UIButton!
+    private let patternChecker: PatternChecker = PatternChecker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureInputViews()
+    }
+}
+
+extension SignUpViewController: InputViewDelegate {
+    func textDidChanged(_ inputView: InputView, text: String) {
+        switch inputView {
+        case emailInputView:
+            inputView.render(with: patternChecker.isValid(email: text))
+        case passwordInputView:
+            inputView.render(with: patternChecker.isValid(passWord: text))
+            passwordConfirmInputView.render(with: patternChecker.isValid(passwordCheck: nil))
+        case passwordConfirmInputView:
+            inputView.render(with: patternChecker.isValid(passwordCheck: text))
+        case nameInputView:
+            inputView.render(with: patternChecker.isValid(nickName: text))
+        default:
+            break
+        }
+        completeButton.isEnabled = patternChecker.isComplete
+    }
+}
+
+private extension SignUpViewController {
+    func configureInputViews() {
+        emailInputView.delegate = self
+        passwordInputView.delegate = self
+        passwordConfirmInputView.delegate = self
+        nameInputView.delegate = self
     }
 }
