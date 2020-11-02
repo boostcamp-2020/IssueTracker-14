26933d861be89675f69cf52bd519a89b0a3af065
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-02 21:26:32 +0900

    [BE] Feat: label, milestone router 연결

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./user/index");
+const labelRouter = require("./label/index");
+const milestoneRouter = require("./milestone/index");
 
 router.use("/user", userRouter);
+
+router.use("/label", labelRouter);
+router.use("/milestone", milestoneRouter);
 
 module.exports = router;
