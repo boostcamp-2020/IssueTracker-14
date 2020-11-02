4a90216078979cfc3deaf9986e2cb25f0ad95e51
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-02 21:44:48 +0900

    [BE] Fix: label, milestone crud 및 api 수정

--- server/controllers/label_controller.js
+++ server/controllers/label_controller.js
@@ -1,9 +1,8 @@
 const { label: labelModel } = require("../db/models");
 
 const createLabel = async (req, res) => {
-  const { title, description, color } = req.body;
-
   try {
+    const { title, description, color } = req.body;
     const newLabel = await labelModel.create({
       title,
       description,
@@ -21,40 +20,38 @@ const createLabel = async (req, res) => {
 const readLabels = async (req, res) => {
   try {
     const labels = await labelModel.findAll();
-
     if (!Array.isArray(labels)) {
       return res.status(400).json({ message: "fail" });
     }
-
-    return res.status(200).json({ message: "success", data: labels });
+    return res.status(200).json({ message: "success", labels: labels });
   } catch (err) {
     return res.status(400).json({ message: "fail", error: err.message });
   }
 };
 
 const updateLabel = async (req, res) => {
-  const { labelid: id } = req.params;
-  const { title, description, color } = req.body;
-
   try {
+    const { labelid: id } = req.params;
+    const { title, description, color } = req.body;
     await labelModel.update(
       { title, description, color },
       {
-        where: id,
+        where: { id },
       }
     );
+    return res.status(200).json({ message: "success" });
   } catch (err) {
     return res.status(400).json({ message: "fail", error: err.message });
   }
 };
 
 const deleteLabel = async (req, res) => {
-  const { labelid: id } = req.params;
-
   try {
+    const { labelid: id } = req.params;
     await labelModel.destroy({
       where: { id },
     });
+    return res.status(200).json({ message: "success" });
   } catch (err) {
     return res.status(400).json({ message: "fail", error: err.message });
   }
