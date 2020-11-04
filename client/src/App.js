6041fba7f19cc3ba2b84bff6ae3078209bb0c4a0
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-04 21:39:39 +0900

    [FE] Chore: axios 설치

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React, { useEffect } from 'react';
-import LoginForm from './components/organisms/LoginForm';
-import { Link, Switch, Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import LoginForm from "./components/organisms/LoginForm";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledRootContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 const App = () => {
   // TODO: Router 설정 , checkLogin 후 분기
   //useEffect => checkLogin => flase history.push('/login') else history.push('/');
   return (
-    <>
+    <StyledRootContainer>
       <Switch>
         <Route exact path="/" component={LoginForm} />
         <Route exact path="/login" component={LoginForm} />
@@ -21,8 +30,8 @@ const App = () => {
         <Route path="/milestones/new" component={LoginForm} />
         <Route path="/milestones/edit/:milestoneid" component={LoginForm} />
       */}
-    </>
-    )
+    </StyledRootContainer>
+  );
 };
 
 export default App;
