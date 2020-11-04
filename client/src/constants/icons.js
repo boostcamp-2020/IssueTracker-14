58f50a1d953b2975d961ebd63d865565ce9653af
Yu Taek <lcpnine@gmail.com>
2020-11-05 02:28:47 +0900

    [FE] Feat: Icon을 constant 폴더의 상수에 추가하고 atom에서 semantic tag화
    1. Atoms의 Icon에 name을 넣음으로써 필요한 태그 사용 가능
    2. left, right를 정해 필요한 위치에 깃허브를 넣으면서 한 쪽 방향에만 패딩을 추가함으로써 전체 스타일에 영향이 가지 않도록 수정
    3. distance props에 원하는 값을 넣어 다양한 태그 안에 탄력적인 활용 가능

--- client/src/constants/icons.js
+++ client/src/constants/icons.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPassport, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const icons = {
-  github: faGithub,
-  passport: faPassport,
-  codeBranches: faCodeBranch
+  github: <FontAwesomeIcon icon={faGithub} />,
+  passport: <FontAwesomeIcon icon={faPassport} />,
+  codeBranches: <FontAwesomeIcon icon={faCodeBranch} />
 }
 
 export default icons;
