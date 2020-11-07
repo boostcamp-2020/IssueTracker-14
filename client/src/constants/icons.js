f5c6ee8d3480bbe7471507cd3953ab687ef674f9
Yu Taek <lcpnine@gmail.com>
2020-11-08 08:24:31 +0900

    [FE] Style: Header에 필요한 Icon 라이브러리 추가

--- client/src/constants/icons.js
+++ client/src/constants/icons.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPassport, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faPassport, faCodeBranch, faBook } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 
 const icons = {
+  book: <FontAwesomeIcon icon={faBook} />,
   github: <FontAwesomeIcon icon={faGithub} />,
+  githubAlt: <FontAwesomeIcon icon={faGithubAlt} />,
   passport: <FontAwesomeIcon icon={faPassport} />,
   codeBranches: <FontAwesomeIcon icon={faCodeBranch} />
 }
