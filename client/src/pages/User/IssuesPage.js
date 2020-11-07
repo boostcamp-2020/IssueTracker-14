6ea2a860ee8ce99ba0972cf96bb420f2e0cd9e72
Yu Taek <lcpnine@gmail.com>
2020-11-08 08:42:36 +0900

    [FE] Feat: IssuePage에 Header 추가

--- client/src/pages/User/IssuesPage.js
+++ client/src/pages/User/IssuesPage.js
@@ -1,17 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./../../components/organisms/Header";
 
 const IssuesPageWrapper = styled.div`
+  position: relative;
+  top: 6rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 930px;
 `;
 
 const IssuesPage = () => (
-  <IssuesPageWrapper>Issue가 들어올 자리</IssuesPageWrapper>
+  <>
+    <Header />
+    <IssuesPageWrapper>
+      <div>
+        Issue바디 페이지
+      </div>
+    </IssuesPageWrapper>
+  </>
 );
 
 export default IssuesPage;
