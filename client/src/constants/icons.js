e4ebb734409182dc1489106cb4d4d1f8bdd1eb85
Yu Taek <lcpnine@gmail.com>
2020-11-08 15:30:02 +0900

    [FE] Chore: Filter Reset에 사용될 Icon 추가

--- client/src/constants/icons.js
+++ client/src/constants/icons.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPassport, faCodeBranch, faBook, faMapSigns, faTags, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faCodeBranch, faBook, faMapSigns, faTags, faCaretDown, faWindowClose } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 
 const icons = {
   book: <FontAwesomeIcon icon={faBook} />,
   github: <FontAwesomeIcon icon={faGithub} />,
   githubAlt: <FontAwesomeIcon icon={faGithubAlt} />,
-  passport: <FontAwesomeIcon icon={faPassport} />,
   codeBranches: <FontAwesomeIcon icon={faCodeBranch} />,
   milestone: <FontAwesomeIcon icon={faMapSigns} />,
   label: <FontAwesomeIcon icon={faTags} />,
-  dropdown: <FontAwesomeIcon icon={faCaretDown} />
+  dropdown: <FontAwesomeIcon icon={faCaretDown} />,
+  reset: <FontAwesomeIcon icon={faWindowClose} />,
 }
 
 export default icons;
