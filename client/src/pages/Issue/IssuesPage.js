c111c5d00a8d9450b23f176e57e2fe5d9787ddee
Yu Taek <lcpnine@gmail.com>
2020-11-08 18:01:40 +0900

    [FE] Refactor: NavigationWrapper를 IssuePage에 포함시키는 것으로 수정

--- client/src/pages/Issue/IssuesPage.js
+++ client/src/pages/Issue/IssuesPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../../components/organisms/Header";
-import NavigationWrapper from "../../components/organisms/NavigationWrapper";
-import IssueWrapper from "../../components/organisms/IssueWrapper";
+import NavigationWrapperInput from "./../../components/organisms/NavigationWrapperInput";
+import NavigationWrapperLink from "./../../components/molecules/NavigationWrapperLink";
+import NewIssueButton from "./../../components/molecules/NewIssueButton";
+import IssueWrapper from "./../../components/organisms/IssueWrapper";
 
 const IssuesPageWrapper = styled.div`
   position: relative;
@@ -14,11 +16,24 @@ const IssuesPageWrapper = styled.div`
   width: 80%;
 `;
 
+const StyledNavigationWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 1.5rem 0rem 0.5rem 0rem;
+`;
+
 const IssuesPage = () => (
   <>
     <Header />
     <IssuesPageWrapper>
-      <NavigationWrapper />
+      <StyledNavigationWrapper>
+          <NavigationWrapperInput />
+          <NavigationWrapperLink />
+          <NewIssueButton />
+      </StyledNavigationWrapper>
       <IssueWrapper />
     </IssuesPageWrapper>
   </>
