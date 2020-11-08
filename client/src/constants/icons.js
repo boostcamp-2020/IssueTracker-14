0e3f9e8554802bcf2eb71a75ae8e848050939170
Yu Taek <lcpnine@gmail.com>
2020-11-08 12:07:10 +0900

    [FE] Style: Label, MileStone, Dropdown에 사용될 아이콘 추가

--- client/src/constants/icons.js
+++ client/src/constants/icons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPassport, faCodeBranch, faBook } from '@fortawesome/free-solid-svg-icons'
+import { faPassport, faCodeBranch, faBook, faMapSigns, faTags, faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 
 const icons = {
@@ -8,7 +8,10 @@ const icons = {
   github: <FontAwesomeIcon icon={faGithub} />,
   githubAlt: <FontAwesomeIcon icon={faGithubAlt} />,
   passport: <FontAwesomeIcon icon={faPassport} />,
-  codeBranches: <FontAwesomeIcon icon={faCodeBranch} />
+  codeBranches: <FontAwesomeIcon icon={faCodeBranch} />,
+  milestone: <FontAwesomeIcon icon={faMapSigns} />,
+  label: <FontAwesomeIcon icon={faTags} />,
+  dropdown: <FontAwesomeIcon icon={faCaretDown} />
 }
 
 export default icons;
