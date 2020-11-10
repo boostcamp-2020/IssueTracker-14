c49b0f4aeef7b1fd57aea0bd8ff2d7ed346b851d
TTOzzi <devttozzi@gmail.com>
2020-11-10 21:38:00 +0900

    [iOS] Feat: 레이블 관리 화면 UI 구현
    레이블 데이터를 나타낼 Label 구조체 구현, DiffableDataSource 적용을 위한 Hashable 채택
    레이블을 표시할 커스텀 셀 구현, accessories 표시를 위해 UICollectionViewListCell 상속
    UICollectionViewCompositionalLayout을 활용한 리스트 형태의 UI 구현
    UICollectionViewDiffableDataSource 적용
    
    #161

--- iOS/IssueTracker/IssueTracker/LabelListScene/LabelListViewController.swift
+++ iOS/IssueTracker/IssueTracker/LabelListScene/LabelListViewController.swift
@@ -12,13 +12,89 @@ final class LabelListViewController: UIViewController {
     static var identifier: String {
         return String(describing: Self.self)
     }
+    @IBOutlet private weak var labelCollectionView: UICollectionView!
     weak var coordinator: LabelCoordinator?
+    private var labels: [Label] = [
+        Label(id: 1, title: "hello", color: "", description: "asdf"),
+        Label(id: 2, title: "asdf", color: "", description: "vzxcvz"),
+        Label(id: 3, title: "afewfwa", color: "", description: "asdfasdfasdf"),
+        Label(id: 4, title: "ff", color: "", description: nil)
+    ] {
+        didSet {
+            updateList()
+        }
+    }
+    private lazy var dataSource: LabelCollectionViewDataSource = labelDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        updateList()
+    }
+}
 
-        navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
+private extension LabelListViewController {
+    @objc func addButtonDidTouchUp() {
+        coordinator?.showEdit()
+    }
+}
+
+private extension LabelListViewController {
+    func labelCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+    }
+}
+
+private extension LabelListViewController {
+    enum Section {
+        case main
+    }
+    
+    typealias LabelCollectionViewDataSource = UICollectionViewDiffableDataSource<Section, Label>
+    typealias LabelCollectionViewSnapshot = NSDiffableDataSourceSnapshot<Section, Label>
+    
+    func labelDataSource() -> LabelCollectionViewDataSource {
+        return LabelCollectionViewDataSource(
+            collectionView: labelCollectionView,
+            cellProvider: { collectionView, indexPath, label -> LabelCollectionViewCell? in
+                let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: LabelCollectionViewCell.identifier,
+                    for: indexPath
+                ) as? LabelCollectionViewCell
+                cell?.update(with: label)
+                return cell
+            }
+        )
+    }
+    
+    func updateList() {
+        var snapshot = LabelCollectionViewSnapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(labels, toSection: .main)
+        DispatchQueue.main.async { [weak self] in
+            self?.dataSource.apply(snapshot)
+        }
+    }
+}
+
+private extension LabelListViewController {
+    func configure() {
+        configureNavigationBar()
+        configureCollectionView()
+    }
+    
+    func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(addButtonDidTouchUp))
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.topItem?.title = "레이블"
+    }
+    
+    func configureCollectionView() {
+        labelCollectionView.dataSource = dataSource
+        labelCollectionView.setCollectionViewLayout(labelCollectionViewLayout(), animated: true)
+        labelCollectionView.allowsSelection = false
     }
 }
