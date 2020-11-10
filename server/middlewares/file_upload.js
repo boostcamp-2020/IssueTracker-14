5abf80ec40f70c2e213a6fd7a75476b9daf28ef5
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-11 01:16:50 +0900

    [BE] Feat: file upload api 작성

---
+++ server/middlewares/file_upload.js
@@ -0,0 +1,33 @@
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+
+const endpoint = new AWS.Endpoint("https://kr.object.ncloudstorage.com");
+const region = "kr-standard";
+const access_key = process.env.ACCESS_KEY;
+const secret_key = process.env.SECRET_KEY;
+
+const S3 = new AWS.S3({
+  endpoint,
+  region,
+  credentials: {
+    accessKeyId: access_key,
+    secretAccessKey: secret_key,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: S3, // 위에서 정의한 S3 Object
+    bucket: "issue", // 버킷 이름
+    contentType: multerS3.AUTO_CONTENT_TYPE, // 자동을 콘텐츠 타입 세팅
+    acl: "public-read", // 클라이언트에서 자유롭게 가용하기 위함
+    key: (req, file, cb) => {
+      cb(null, new Date().valueOf() + path.extname(file.originalname));
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 용량 제한
+});
+
+module.exports = { upload };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,12 +6,17 @@ const issuesRouter = require("./issue/index");
 const labelRouter = require("./label/index");
 const milestoneRouter = require("./milestone/index");
 
+const fileuploadController = require("../controllers/file_upload_controller");
+
 const { isAuth } = require("../middlewares/auth");
+const { upload } = require("../middlewares/file_upload");
 
 router.use("/user", userRouter);
 
 router.use("/issues", isAuth, issuesRouter);
 router.use("/label", isAuth, labelRouter);
 router.use("/milestone", isAuth, milestoneRouter);
+
+router.post("/fileupload", upload.single("filename"), fileuploadController);
 
 module.exports = router;
