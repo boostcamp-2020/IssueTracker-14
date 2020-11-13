f622bd7ba6519c9f522596a212f1d0aec044cb7c
Yu Taek <lcpnine@gmail.com>
2020-11-13 10:30:54 +0900

    [FE] Feat: Webpack Alias 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const Dotenv = require("dotenv-webpack");
 
 const isDevelopment = process.env.NODE_ENV === "development";
@@ -10,6 +11,15 @@ module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
   resolve: {
     extensions: [".js", ".jsx"],
+    alias: {
+      "@atoms": path.resolve(___dirname, "src/components/atoms"),
+      "@molecules": path.resolve(___dirname, "src/components/molecules"),
+      "@organisms": path.resolve(___dirname, "src/components/organisms"),
+      "@constants": path.resolve(___dirname, "src/constants"),
+      "@pages": path.resolve(___dirname, "src/pages"),
+      "@stores": path.resolve(___dirname, "src/stores"),
+      "@utils": path.resolve(___dirname, "src/utils"),
+    }
   },
   devtool: "eval-cheap-source-map",
   devServer: {
@@ -44,6 +54,7 @@ module.exports = {
     new CleanWebpackPlugin({
       cleanAfterEveryBuildPatterns: ["dist"],
     }),
+    new FaviconsWebpackPlugin("./octocat.png"),
     new Dotenv(),
   ],
 };
